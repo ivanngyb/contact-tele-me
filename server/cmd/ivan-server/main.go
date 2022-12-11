b2b427cffb534b3a4eef6df23de1255cf1aa3cf3
ivanngyb <ivanng2@hotmail.com>
2022-12-11 22:40:12 +0800

    Setup logger

--- server/cmd/ivan-server/main.go
+++ server/cmd/ivan-server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"server/api"
+	"server/logger"
 
 	"github.com/urfave/cli/v2"
 )
@@ -19,7 +20,9 @@ func main() {
 				Name:    "serve",
 				Aliases: []string{"s"},
 				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "environment", Value: "development", EnvVars: []string{envPrefix + "_environment"}, Usage: "Environment as to what the server is running in"},
 					&cli.StringFlag{Name: "api_addr", Value: ":8084", EnvVars: []string{envPrefix + "_API_ADDR", "API_ADDR"}, Usage: "Port for which server will run on"},
+					&cli.StringFlag{Name: "log_level", Value: "DebugLevel", EnvVars: []string{envPrefix + "_LOG_LEVEL", "LOG_LEVEL"}, Usage: "Log level for logger"},
 				},
 				Usage: "Run server",
 				Action: func(ctx *cli.Context) error {
@@ -27,6 +30,9 @@ func main() {
 						Address: ctx.String("api_addr"),
 					}
 
+					logger.NewLogger(ctx.String("environment"), ctx.String("log_level"))
+
+					logger.L.Info().Msg("Starting API Server")
 					api := api.NewAPI(ctx.Context, apiConfig)
 
 					err := api.Run(ctx.Context)
